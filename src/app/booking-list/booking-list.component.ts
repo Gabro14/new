8b925371acfcce09e282bfeb99adfe1b13007e93
Default <xbotandspacemorty@gmail.com>
2025-05-24 15:41:06 +0400

    new

--- src/app/booking-list/booking-list.component.ts
+++ src/app/booking-list/booking-list.component.ts
@@ -1,37 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
+// import { Component, OnInit } from '@angular/core';
+// import { ApiService } from '../api.service';
 
-@Component({
-  selector: 'app-booking-list',
-  templateUrl: './booking-list.component.html',
-  styleUrls: ['./booking-list.component.scss']
-})
-export class BookingListComponent implements OnInit {
-  bookings: any[] = [];
-  loading = true;
+// @Component({
+//   selector: 'app-booking-list',
+//   templateUrl: './booking-list.component.html',
+//   styleUrls: ['./booking-list.component.scss']
+// })
+// export class BookingListComponent implements OnInit {
+//   bookings: any[] = [];
+//   loading = true;
 
-  constructor(private apiService: ApiService) {}
+//   constructor(private apiService: ApiService) {}
 
-  ngOnInit(): void {
-    this.fetchBookings();
-  }
+//   ngOnInit(): void {
+//     this.fetchBookings();
+//   }
 
-  fetchBookings() {
-    this.loading = true;
-    this.apiService.getAllBookings().subscribe({
-      next: (resp: any[]) => {
-        this.bookings = resp;
-        this.loading = false;
-      },
-      error: () => this.loading = false
-    });
-  }
+//   fetchBookings() {
+//     this.loading = true;
+//     this.apiService.getAllBookings().subscribe({
+//       next: (resp: any[]) => {
+//         this.bookings = resp;
+//         this.loading = false;
+//       },
+//       error: () => this.loading = false
+//     });
+//   }
 
-  deleteBooking(id: string) {
-    if (confirm('დარწმუნებული ხარ რომ გინდა წაშლა?')) {
-      this.apiService.deleteBooking(id).subscribe(() => {
-        this.bookings = this.bookings.filter(b => b.id !== id);
-      });
-    }
-  }
-}
+//   deleteBooking(id: string) {
+//     if (confirm('დარწმუნებული ხარ რომ გინდა წაშლა?')) {
+//       this.apiService.deleteBooking(id).subscribe(() => {
+//         this.bookings = this.bookings.filter(b => b.id !== id);
+//       });
+//     }
+//   }
+// }
